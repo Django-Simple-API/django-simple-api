118fe2ac2b446c6cd70994be9721af9744586228
abersheeran <me@abersheeran.com>
2020-12-15 22:03:52 +0800

    Update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,8 +4,6 @@ from django.http.request import QueryDict
 from django_simple_api.utils import _merge_query_dict
 
 
-@pytest.mark.parametrize("query_dict,result", [
-    (QueryDict(mutable=True), {})
-])
+@pytest.mark.parametrize("query_dict,result", [(QueryDict(mutable=True), {})])
 def test_merge_query_dict(query_dict, result):
     assert _merge_query_dict(query_dict) == result
