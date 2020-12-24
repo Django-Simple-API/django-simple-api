8da85703ee4c0b1b4dab1dcad734d56a903a79be
guo junjie <121785021@qq.com>
2020-12-24 20:30:29 +0800

    url format openapi3

--- django_simple_api/utils.py
+++ django_simple_api/utils.py
@@ -1,8 +1,35 @@
+import re
 import typing
 
 from django.conf import settings
 from django.urls import URLPattern, URLResolver
 from django.http.request import QueryDict
+
+CONVERT_REG = [
+    r"\^?([a-zA-Z0-9_]+)\$?",  # /articles
+    r"\^?<([a-zA-Z0-9_]+)>\$?",  # /<name>
+    r"\^?<[a-zA-Z]+:([a-zA-Z0-9_]+)>\$?",  # /<int:id>
+    r"\^?\(\?P<(.*?)>.*?\)\$?",  # '^(?P<app_label>auth)'
+]
+
+
+def convert_url(url_pattern: URLPattern) -> typing.Generator[str, None, None]:
+    path_list: typing.List[str] = str(url_pattern.pattern).split("/")
+    for path in path_list:
+        if path in ["", "$"]:
+            yield ""
+            continue
+        for idx, reg in enumerate(CONVERT_REG):
+            match_res = re.match(reg, path)
+            if match_res is None:
+                continue
+            if idx == 0:
+                yield match_res.group(1)
+            else:
+                yield "{%s}" % match_res.group(1)
+            break
+        else:
+            yield path
 
 
 def get_urls() -> typing.Generator[typing.Tuple[str, typing.Any], None, None]:
@@ -16,10 +43,9 @@ def get_urls() -> typing.Generator[typing.Tuple[str, typing.Any], None, None]:
             return
         item = lis[0]
         if isinstance(item, URLPattern):
-            # TODO 将 path 转换为 openapi3 标准格式
-            # <id> -> {id}
-            # <path:object_id> -> {object_id}
-            yield prefix + str(item.pattern), item.callback
+            # 将 path 转换为 openapi3 标准格式
+            print("---contrast: " + prefix + str(item.pattern))
+            yield prefix + "/".join(convert_url(item)), item.callback
         elif isinstance(item, URLResolver):
             yield from _(item.url_patterns, prefix + str(item.pattern))
         yield from _(lis[1:], prefix)
