c449b07a5dd1d2d8b5f24bf76bda02e056f3441c
Aber <me@abersheeran.com>
2021-01-20 16:31:19 +0800

    Add more test

--- example/tests.py
+++ example/tests.py
@@ -10,3 +10,15 @@ class TestJustTest(TestCase):
     def test_failed_get(self):
         resp = self.client.get("/just-test/abc")
         self.assertEqual(resp.status_code, 422)
+
+    def test_success_post(self):
+        resp = self.client.post("/just-test/1", data={"name_id": 1})
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content, b"2")
+
+    def test_failed_post(self):
+        resp = self.client.post("/just-test/abc")
+        self.assertEqual(resp.status_code, 422)
+
+        resp = self.client.post("/just-test/1", data={"name_id": "abc"})
+        self.assertEqual(resp.status_code, 422)
--- example/views.py
+++ example/views.py
@@ -7,3 +7,6 @@ from django_simple_api import Path, Query, Header, Cookie, Body, Exclusive
 class JustTest(View):
     def get(self, request, id: int = Path(...)):
         return HttpResponse(id)
+
+    def post(self, request, id: int = Path(...), name_id: int = Body(...)):
+        return HttpResponse(id + name_id)
