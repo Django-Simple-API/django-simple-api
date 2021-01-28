ebe214aac45cb9e5abce3771cbc026f19b447b2e
abersheeran <me@abersheeran.com>
2021-01-28 19:42:44 +0800

    Fix merge http header error

--- django_simple_api/utils.py
+++ django_simple_api/utils.py
@@ -43,8 +43,13 @@ def get_all_urls() -> Generator[Tuple[str, Any], None, None]:
 def merge_query_dict(query_dict: QueryDict) -> dict:
     return {
         k: v if len(v) > 1 else v[0]
-        for k, v in map(lambda k: (k, query_dict.getlist(k)), query_dict.keys())
-        if v
+        for k, v in filter(
+            lambda kv: kv[1],
+            map(
+                lambda k: (k, query_dict.getlist(k)),
+                query_dict.keys(),
+            ),
+        )
     }
 
 
