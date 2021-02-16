e25048787900019626f3e468cc1d0b11461be7e2
Aber <me@abersheeran.com>
2021-02-16 19:58:11 +0800

    Added script/docs.py for build and deploy site (#34)

---
+++ script/docs.py
@@ -0,0 +1,21 @@
+import os
+from subprocess import check_output as shell
+from pathlib import Path
+
+commit_id = shell("git rev-parse HEAD").decode("utf8").strip()
+site = Path("site").absolute()
+
+shell(f"mkdocs build --clean --site-dir {site}")
+
+os.chdir(site)
+cname = site / "CNAME"
+cname.touch()
+cname.write_text("django-simple-api.aber.sh")
+
+shell("git init -q")
+shell("git add . ")
+shell(f'git commit -m "Build by {commit_id}"')
+shell(
+    "git remote add origin https://github.com/Django-Simple-API/django-simple-api.github.io"
+)
+shell("git push --force origin master")
