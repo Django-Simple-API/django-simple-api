3428f0c3143b2a51d77f5b0cd736cc6af068e575
chen <810975099@qq.com>
2021-04-09 09:43:56 +0800

    fix testcase...

--- tests/tests/test_parameter_declare.py
+++ tests/tests/test_parameter_declare.py
@@ -1,3 +1,5 @@
+import pytest
+
 from django.http import HttpResponse
 
 from pydantic import BaseModel, Field
@@ -12,19 +14,19 @@ class QueryPage(BaseModel):
     num: int = Field(1, alias="page-num")
 
 
-def just_view_1(request, p1: int = Query(exclusive=True)):
+def test_view_1(request, p1: int = Query(exclusive=True)):
     return HttpResponse()
 
 
-def just_view_2(request, p1=Query(exclusive=True)):
+def test_view_2(request, p1=Query(exclusive=True)):
     return HttpResponse()
 
 
-def just_view_3(request, p1: QueryPage = Query(exclusive=True), p2: int = Query()):
+def test_view_3(request, p1: QueryPage = Query(exclusive=True), p2: int = Query()):
     return HttpResponse()
 
 
-def just_view_4(
+def test_view_4(
     request,
     p1: int = Query(),
     p2: QueryPage = Query(exclusive=True),
@@ -32,67 +34,51 @@ def just_view_4(
     return HttpResponse()
 
 
-def just_view_5(
-    request,
-    p1: QueryPage = Query(exclusive=True, default="1"),
-):
-    return HttpResponse()
-
-
 class TestParameterDeclare:
     def test_parameter_declare_1(self):
-        try:
-            parse_and_bound_params(just_view_1)
-        except TypeError as e:
-            assert (
-                str(e)
-                == "The `p1` parameter of `just_view_1` must use type annotations and "
-                "the type annotations must be a subclass of BaseModel."
-            )
-        else:
-            raise AssertionError("Expected exception did not occur!")
+        with pytest.raises(TypeError) as e:
+            parse_and_bound_params(test_view_1)
+        assert (
+            str(e.value)
+            == "The `p1` parameter of `test_view_1` must use type annotations and "
+            "the type annotations must be a subclass of BaseModel."
+        )
 
     def test_parameter_declare_2(self):
-        try:
-            parse_and_bound_params(just_view_2)
-        except TypeError as e:
-            assert (
-                str(e)
-                == "The `p1` parameter of `just_view_2` must use type annotations and "
-                "the type annotations must be a subclass of BaseModel."
-            )
-        else:
-            raise AssertionError("Expected exception did not occur!")
+        with pytest.raises(TypeError) as e:
+            parse_and_bound_params(test_view_2)
+        assert (
+            str(e.value)
+            == "The `p1` parameter of `test_view_2` must use type annotations and "
+            "the type annotations must be a subclass of BaseModel."
+        )
 
     def test_parameter_declare_3(self):
-        try:
-            parse_and_bound_params(just_view_3)
-        except ExclusiveFieldError as e:
-            assert (
-                str(e) == "You used exclusive parameter: `Query(exclusive=True)`, "
-                "Please ensure the `Query` field is unique in `just_view_3`."
-            )
-        else:
-            raise AssertionError("Expected exception did not occur!")
+        with pytest.raises(ExclusiveFieldError) as e:
+            parse_and_bound_params(test_view_3)
+        assert (
+            str(e.value) == "You used exclusive parameter: `Query(exclusive=True)`, "
+            "Please ensure the `Query` field is unique in `test_view_3`."
+        )
 
     def test_parameter_declare_4(self):
-        try:
-            parse_and_bound_params(just_view_4)
-        except ExclusiveFieldError as e:
-            assert (
-                str(e) == "You used exclusive parameter: `Query(exclusive=True)`, "
-                "Please ensure the `Query` field is unique in `just_view_4`."
-            )
-        else:
-            raise AssertionError("Expected exception did not occur!")
+        with pytest.raises(ExclusiveFieldError) as e:
+            parse_and_bound_params(test_view_4)
+        assert (
+            str(e.value) == "You used exclusive parameter: `Query(exclusive=True)`, "
+            "Please ensure the `Query` field is unique in `test_view_4`."
+        )
 
     def test_parameter_declare_5(self):
-        try:
-            parse_and_bound_params(just_view_5)
-        except ExclusiveFieldError as e:
-            assert (
-                str(e)
-                == "The `exclusive=True` parameter cannot be used with other parameters at the same time."
-            )
-        else:
-            raise AssertionError("Expected exception did not occur!")
+        with pytest.raises(ExclusiveFieldError) as e:
+            
+            def test_view(
+                request,
+                p1: QueryPage = Query(exclusive=True, title="1"),
+            ):
+                return HttpResponse()
+            
+        assert (
+            str(e.value)
+            == "The `exclusive=True` parameter cannot be used with other parameters at the same time."
+        )
