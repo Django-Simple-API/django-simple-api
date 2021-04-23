06a5ff0f816840885f70e33c19ca1a43d3c23350
chen <810975099@qq.com>
2021-04-23 16:09:01 +0800

    docs.py compatible with macos

--- script/docs.py
+++ script/docs.py
@@ -2,20 +2,19 @@ import os
 from pathlib import Path
 from subprocess import check_output as shell
 
-commit_id = shell("git rev-parse HEAD").decode("utf8").strip()
+commit_id = shell("git rev-parse HEAD", shell=True).decode("utf8").strip()
 site = Path("site").absolute()
 
-shell(f"mkdocs build --clean --site-dir {site}")
+shell(f"mkdocs build --clean --site-dir {site}", shell=True)
 
 os.chdir(site)
 cname = site / "CNAME"
 cname.touch()
 cname.write_text("django-simple-api.aber.sh")
 
-shell("git init -q")
-shell("git add . ")
-shell(f'git commit -m "Build by {commit_id}"')
+shell("git init -q", shell=True)
+shell("git add . ", shell=True)
+shell(f'git commit -m "Build by {commit_id}"', shell=True)
 shell(
-    "git remote add origin https://github.com/Django-Simple-API/django-simple-api.github.io"
-)
-shell("git push --force origin master")
+    "git remote add origin https://github.com/Django-Simple-API/django-simple-api.github.io", shell=True)
+shell("git push --force origin master", shell=True)
